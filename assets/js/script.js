9e78b2e45da90cf9af3de8c3f84784703a2fb8a2
Ashley Waters <aishelweinberg@gmail.com>
2022-03-31 22:06:44 -0400

    spent an hour wondering why js isn't working and it's because i didn't link it in html

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,23 @@ var searchHistoryList = $("#search-history-list");
 var searchCityInput = $("search-city");
 var searchCityButton = $("search-city-button");
 var clearHistoryButton = $("#clear-history");
+
+var currentCity = $("#current-city");
+var currentTemp = $("#current-temp");
+var currentHumidity = $("current-humidity");
+var currentWindSpeed = $("current-wind-speed");
+var UVindex = $("#uv-index");
+
+var weatherContent = $("#weather-content");
+
+// api key for openweather
+var APIkey = "a626c658552f289834580d6d2e3d0bb6";
+    console.log(APIkey);
+
+// access to data
+var cityList = [];
+
+// current date and display in title
+var currentDate = moment().format('L');
+$("#current-date").text("(" + currentDate +")");
+    console.log(currentDate);
