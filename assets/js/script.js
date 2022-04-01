37c1b9b384099277abe7732dd04671b9143403eb
Aishels <aishelweinberg@gmail.com>
2022-04-01 00:09:20 -0400

    added in functions for cities in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,5 +21,34 @@ var cityList = [];
 // current date and display in title
 var currentDate = moment().format('L');
 $("#current-date").text("(" + currentDate +")");
-    console.log(currentDate);
+// console.log(currentDate);
 
+// clear history
+
+// search button click listener
+searchCityButton.on("click", function(event){
+    event.preventDefault();
+
+    var searchValue = searchCityInput.val().trim();
+
+    currentConditionsRequest(searchValue);
+    searchHistory(searchValue);
+    searchCityInput.val("");
+})
+
+// api based on user input
+function currentConditionsRequest(searchValue) {
+    var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&units=imperial&appid=" + APIkey;
+    
+}
+
+
+
+
+function initalizeHistory() {
+    if(localStorage.getItem("cities")) {
+        cityList = JSON.parse(localStorage.getItem("cities"));
+        var lastIndex = cityList.length - 1;
+        console.log(cityList);
+    }
+}
