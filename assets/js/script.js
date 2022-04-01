0777dcae9ae1c0a2d7a4e0877f792051054b2cc7
Ashley Waters <aishelweinberg@gmail.com>
2022-04-01 00:48:29 -0400

    changed around js to add in fetch commands for api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,52 @@
-var searchHistoryList = $("#search-history-list");
-var searchCityInput = $("search-city");
-var searchCityButton = $("search-city-button");
-var clearHistoryButton = $("#clear-history");
+// global variables
+var apiKey = "a626c658552f289834580d6d2e3d0bb6";
+var savedSearches = [];
 
-var currentCity = $("#current-city");
-var currentTemp = $("#current-temp");
-var currentHumidity = $("current-humidity");
-var currentWindSpeed = $("current-wind-speed");
-var UVindex = $("#uv-index");
+var searchHistoryList = function(cityName) {
+    $('.past-search:content("' + cityName + '")').remove();
 
-var weatherContent = $("#weather-content");
 
-// api key for openweather
-var APIkey = "a626c658552f289834580d6d2e3d0bb6";
-    console.log(APIkey);
+}
 
-// access to data
-var cityList = [];
+var currentWeatherSection = function(cityName) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(response) {
+            // longitude and latitude
+            var cityLon = response.coord.lon;
+            var cityLat = response.coord.lat;
 
-// current date and display in title
-var currentDate = moment().format('L');
-$("#current-date").text("(" + currentDate +")");
-// console.log(currentDate);
+            fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLon}&exclude=minutely,hourly,alerts&units=imperial&appid=${apiKey}`)
+                .then(function(response) {
+                    return response.json();
+            })
+                .then(function(response){
+                    searchHistoryList(cityName);
+                    
+                })
+        })
+}
 
-// clear history
 
-// search button click listener
-searchCityButton.on("click", function(event){
+$("#search-form").on("submit", function() {
     event.preventDefault();
-
-    var searchValue = searchCityInput.val().trim();
-
-    currentConditionsRequest(searchValue);
-    searchHistory(searchValue);
-    searchCityInput.val("");
-})
-
-// api based on user input
-function currentConditionsRequest(searchValue) {
-    var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&units=imperial&appid=" + APIkey;
     
-}
+    var cityName = $("#search-input").val();
 
-
-
-
-function initalizeHistory() {
-    if(localStorage.getItem("cities")) {
-        cityList = JSON.parse(localStorage.getItem("cities"));
-        var lastIndex = cityList.length - 1;
-        console.log(cityList);
+    if (cityName === "" || cityName == null) {
+        alert("Please enter name of city.");
+        event.preventDefault();
+    } else {
+        currentWeatherSection(cityName);
+        fiveDayForecastSection(cityName);
     }
-}
+});
+
+
+// $("#search-history-container").on("click", "p", function() {
+//     var previousCityName = $(this).text();
+//     currentWeatherSection(previousCityName);
+    
+// })
