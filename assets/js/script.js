22e241a54ea917724a76cf34ec912b84c75519ab
Ashley Waters <aishelweinberg@gmail.com>
2022-04-01 01:04:43 -0400

    added in jquery script file to html because it was forgotten

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,33 @@ var apiKey = "a626c658552f289834580d6d2e3d0bb6";
 var savedSearches = [];
 
 var searchHistoryList = function(cityName) {
-    $('.past-search:content("' + cityName + '")').remove();
+    $('.past-search:contains("' + cityName + '")').remove();
+
+    var searchHistoryEntry = $("<p>");
+    searchHistoryEntry.addClass("past-search");
+    searchHistoryEntry.text(cityName);
+
+    var searchEntryContainer = $("<div>");
+    searchEntryContainer.addClass("past-search-container");
+
+    searchEntryContainer.append(searchHistoryEntry);
+
+    var searchHistoryContainerEl = $("#search-history-container");
+    searchHistoryContainerEl.append(searchEntryContainer);
+
+    if (savedSearches.length > 0){
+        var previousSavedSearches = localStorage.getItem("savedSearches");
+        savedSearches = JSON.parse(previousSavedSearches);
+    }
+
+    savedSearches.push(cityName);
+    localStorage.setItem("savedSearches", JSON.stringify(savedSearches));
+
+    $("#search-input").val("");
+
+};
 
 
-}
 
 var currentWeatherSection = function(cityName) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`)
@@ -25,9 +48,36 @@ var currentWeatherSection = function(cityName) {
                 .then(function(response){
                     searchHistoryList(cityName);
                     
+                    var currentWeatherSection = $("#current-weather-container");
+                    currentWeatherContainer.addClass("current-weather-container");
+
+                    var currentTitle = $("current-title");
+                    var currentDay = moment().format("M/D/YYYY");
+                    currentTitle.text(`${cityName} (${currentDay})`);
+                    var currentIcon = $("#current-weather-icon");
+                    currentIcon.addClass("current-weather-icon");
+                    var currentIconCode = response.current.weather[0].icon;
+
+
+
+                    if (response.currentuvi <= 2) {
+                        currentNumber.addClass("favorable");
+                    } else if (response.current.uvi >= 3 && response.current.uvi <= 7) {
+                        currentNumber.addClass("moderate");
+                    } else {
+                        currentNumber.addClass("severe");
+                    }
+                    
                 })
         })
+        .catch(function(err) {
+            $("search-input").val("");
+            
+            alert("We could not find the city you searched for. Try searching for a valid city.");
+        })
 }
+
+
 
 
 $("#search-form").on("submit", function() {
